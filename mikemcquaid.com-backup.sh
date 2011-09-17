00b11ba3f47580e592b29bac70a4f2678e3f4379
Mike McQuaid <mike@mikemcquaid.com>
2011-09-17 14:21:39 +0100

    Stop backing up non-existent work directory.

--- mikemcquaid.com-backup.sh
+++ mikemcquaid.com-backup.sh
@@ -10,10 +10,6 @@ REMOTE=/home/mike/Documents/Backup/Remote
 cd /var/www/
 tar jvcf $LOCAL/wordpress.tar.bz2 *
 
-# Backup home directory web files
-cd /home/mike/Sites/work/
-tar jvcf $LOCAL/work.tar.bz2 *
-
 # Backup Wordpress database
 USERNAME=$(grep DB_USER /var/www/wp-config.php | awk -F "'" '{print $4}')
 DATABASE=$(grep DB_NAME /var/www/wp-config.php | awk -F "'" '{print $4}')
@@ -27,6 +23,5 @@ tar jvcf $LOCAL/git-private.tar.bz2 *
 
 # Copy to NFS backup share
 cp -v $LOCAL/wordpress.tar.bz2 		$REMOTE
-cp -v $LOCAL/work.tar.bz2 		$REMOTE
 cp -v $LOCAL/wordpress.sql.bz2 		$REMOTE
 cp -v $LOCAL/git-private.tar.bz2 	$REMOTE
