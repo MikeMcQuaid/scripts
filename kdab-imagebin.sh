092eb44fd1940ccbf673af9780755773d9d84471
Mike McQuaid <mike@mikemcquaid.com>
2011-09-23 12:21:36 +0100

    Move KDAB scripts to KDAB repository.

--- kdab-imagebin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FILE="$1"
-USER=$(whoami)
-PASSWORD=$(security 2>&1 find-internet-password -gs imagebin.kdab.com | grep password | cut -f2 -d\")
-
-curl --user $USER:$PASSWORD -F task=post -F name= -F link= -F field1=$USER -F field2= -F field3= -F field4= -F file=@"$FILE" -F password=$PASSWORD -F submit=Submit https://imagebin.kdab.com/wakaba.pl
-
-open https://imagebin.kdab.com/
