5b72e694facd2b30d1e770ff4ad9392e054d33b0
Mike McQuaid <mike@mikemcquaid.com>
2011-09-30 12:22:26 +0100

    Stashing breaks things so don't do it.

--- scm-update.sh
+++ scm-update.sh
@@ -23,15 +23,11 @@ do
     if [ -d .git/svn ]
     then
         echorun git svn fetch
-        echorun git stash
         echorun git svn rebase
-        echorun git stash apply
     elif [ -d .git ]
     then
         echorun git fetch --all
-        echorun git stash
         echorun git pull
-        echorun git stash apply
     elif [ -d .svn ]
     then
         echorun svn update
