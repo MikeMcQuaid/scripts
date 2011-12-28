9a700b363ffcd54228a3294c4fa41bc28128b063
Mike McQuaid <mike@mikemcquaid.com>
2011-12-28 16:13:38 +0000

    Check downloads directory for files.

--- interfacelift-rename.sh
+++ interfacelift-rename.sh
@@ -4,5 +4,9 @@
 # Exit on any command failures
 set -e
 
+mkdir -p ~/Pictures/Wallpapers/
+mv ~/Downloads/*_*_*x*.jpg ~/Pictures/Wallpapers/
+
+cd ~/Pictures/Wallpapers/
 autoload zmv
 zmv -v '*_(*)_*x*.jpg' '$1.jpg'
