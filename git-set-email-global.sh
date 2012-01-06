c9fd29ffd2201158dfd14700b2b40956fe3bc7e5
Mike McQuaid <mike@mikemcquaid.com>
2012-01-06 12:30:10 +0000

    Add KDAB email setting script.

---
+++ git-set-email-global.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Set email for all git repositories under the current directory.
+
+EMAIL="mike.mcquaid@kdab.com"
+
+CURRENT=$PWD
+# Set to newline to loop over find output correctly on spaced paths.
+IFS="
+"
+
+for SCM in $(find $CURRENT -name .git)
+do
+    DIRECTORY=$(dirname $SCM)
+    cd $DIRECTORY
+    if [ -d .git ]
+    then
+        if ! git config --local --get user.email >/dev/null
+        then
+           echo == Setting email to $EMAIL for $(basename $DIRECTORY)
+           git config --local --add user.email $EMAIL
+        fi
+    fi
+done
