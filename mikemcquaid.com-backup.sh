74210bda96846844a9743e9b06e0325723ead986
Mike McQuaid <mike@mikemcquaid.com>
2012-04-13 12:20:34 +0100

    Not just Wordpress to backup now.

--- mikemcquaid.com-backup.sh
+++ mikemcquaid.com-backup.sh
@@ -8,7 +8,7 @@ REMOTE=/home/mike/Documents/Backup/Remote
 
 # Backup Wordpress files
 cd /var/www/
-tar jvcf $LOCAL/wordpress.tar.bz2 *
+tar jvcf $LOCAL/www.tar.bz2 *
 
 # Backup Wordpress database
 USERNAME=$(grep DB_USER /var/www/wp-config.php | awk -F "'" '{print $4}')
@@ -22,6 +22,6 @@ cd /home/mike/Documents/Git/
 tar jvcf $LOCAL/git-private.tar.bz2 *
 
 # Copy to NFS backup share
-cp -v $LOCAL/wordpress.tar.bz2 		$REMOTE
+cp -v $LOCAL/www.tar.bz2 			$REMOTE
 cp -v $LOCAL/wordpress.sql.bz2 		$REMOTE
 cp -v $LOCAL/git-private.tar.bz2 	$REMOTE
