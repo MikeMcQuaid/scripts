ff81d65b564ae4f96e40853a40ddf5b371c0c410
Mike McQuaid <mike@mikemcquaid.com>
2012-06-01 12:20:01 +0100

    Don't try and backup Wordpress: it is gone.

--- mikemcquaid.com-backup.sh
+++ mikemcquaid.com-backup.sh
@@ -10,18 +10,10 @@ REMOTE=/home/mike/Documents/Backup/Remote
 cd /var/www/
 tar jvcf $LOCAL/www.tar.bz2 *
 
-# Backup Wordpress database
-USERNAME=$(grep DB_USER /var/www/wordpress/wp-config.php | awk -F "'" '{print $4}')
-DATABASE=$(grep DB_NAME /var/www/wordpress/wp-config.php | awk -F "'" '{print $4}')
-PASSWORD=$(grep DB_PASSWORD /var/www/wordpress/wp-config.php | awk -F "'" '{print $4}')
-mysqldump -u "$USERNAME" --database "$DATABASE" --password="$PASSWORD" \
-	| bzip2 -c > $LOCAL/wordpress.sql.bz2
-
 # Backup private Git repositories
 cd /home/mike/Documents/Git/
 tar jvcf $LOCAL/git-private.tar.bz2 *
 
 # Copy to NFS backup share
-cp -v $LOCAL/www.tar.bz2 			$REMOTE
-cp -v $LOCAL/wordpress.sql.bz2 		$REMOTE
+cp -v $LOCAL/www.tar.bz2 		$REMOTE
 cp -v $LOCAL/git-private.tar.bz2 	$REMOTE
