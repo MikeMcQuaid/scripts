a2085a929c5071dbc6e7f9cf3c327f75733f9c7f
Mike McQuaid <mike@mikemcquaid.com>
2012-07-27 22:00:55 +0100

    Add script to uninstall Xcode CLT.

---
+++ uninstall-xcode-clt.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+for FILE in $(pkgutil --only-files --files com.apple.pkg.DeveloperToolsCLI)
+do
+	FILE="/$FILE"
+	[ -f $FILE ] || continue
+
+	echo "Removing file: $FILE"
+	rm -f $FILE
+done
+
+for DIRECTORY in $(pkgutil --only-dirs --files com.apple.pkg.DeveloperToolsCLI)
+do
+	DIRECTORY="/$DIRECTORY"
+	[ -d $DIRECTORY ] || continue
+
+	if [ "$(ls -A $DIRECTORY)" ]
+	then
+		continue
+	fi
+
+	DIRECTORY_REMOVED=1
+	echo "Removing empty directory: $DIRECTORY"
+	rm -rf $DIRECTORY
+done
+
+[ $DIRECTORY_REMOVED ] && exit 1
+pkgutil --forget com.apple.pkg.DeveloperToolsCLI
