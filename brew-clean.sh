eac9bb784e0b828132fa0c491ca87c78c7d490b2
Mike McQuaid <mike@mikemcquaid.com>
2012-08-11 13:43:08 +0100

    brew-list.sh: temporary list shouldn't exist yet.

--- brew-clean.sh
+++ brew-clean.sh
@@ -7,7 +7,6 @@ set -e
 BREW_CLEAN_WHITELIST=~/.brew-clean-whitelist
 [ -s $BREW_CLEAN_WHITELIST ]
 BREW_LIST=$TMPDIR/brew-list
-[ -s $BREW_LIST ]
 
 brew list > $BREW_LIST
 comm -13 $BREW_CLEAN_WHITELIST $BREW_LIST | xargs brew uninstall
