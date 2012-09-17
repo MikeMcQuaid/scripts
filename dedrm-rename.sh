1769c26a1dc824cb13cf0691b54edcea64b9857a
Mike McQuaid <mike@mikemcquaid.com>
2012-09-17 12:14:55 -0700

    Only try and rename .mobi files.

--- dedrm-rename.sh
+++ dedrm-rename.sh
@@ -6,7 +6,7 @@ set -e
 
 mkdir -p ~/Documents/Books/DeDRM/
 cd ~/Documents/Books/DeDRM/
-for OLDFILENAME in *
+for OLDFILENAME in *.mobi
 do
 	NEWFILENAME=$(echo $OLDFILENAME | sed -e 's/[A-Z0-9]*_\(.*\)/\1/' \
 		-e 's/_/ /g' -e 's/EBOK //' -e 's/ nodrm\.mobi/.mobi/' -e 's/  / - /')
