c4eb24600500cb0a8c7409b21839b807173d138e
Mike McQuaid <mike@mikemcquaid.com>
2018-11-06 09:15:10 +0000

    rbenv-sync-homebrew-rubies: handle Homebrew formulae revisions.

--- rbenv-sync-homebrew-rubies.rb
+++ rbenv-sync-homebrew-rubies.rb
@@ -3,14 +3,28 @@
 require "pathname"
 require "fileutils"
 
-rbenv_versions          = Pathname("#{ENV["HOME"]}/.rbenv/versions")
+@rbenv_versions         = Pathname("#{ENV["HOME"]}/.rbenv/versions")
 homebrew_ruby_versions  = Pathname.glob("/usr/local/Cellar/ruby")
 homebrew_ruby_versions += Pathname.glob("/usr/local/Cellar/ruby@*")
 
-homebrew_ruby_versions.flat_map(&:children)
-                      .each {|v| FileUtils.ln_sf v, rbenv_versions }
+def link_rbenv_version_without_revision(version)
+  basename_without_revision = version.basename
+                                     .to_s
+                                     .gsub(/_\d+$/, "")
+  FileUtils.ln_sf version, @rbenv_versions / basename_without_revision
+end
 
-rbenv_versions.children
-              .select(&:symlink?)
-              .reject(&:exist?)
-              .each {|v| FileUtils.rm_f v }
+def gem_like_version(version)
+  Gem::Version.new(version.basename
+                          .to_s
+                          .tr("_", "."))
+end
+
+homebrew_ruby_versions.flat_map(&:children)
+                      .sort_by(&method(:gem_like_version))
+                      .each(&method(:link_rbenv_version_without_revision))
+
+@rbenv_versions.children
+               .select(&:symlink?)
+               .reject(&:exist?)
+               .each { |path| FileUtils.rm_f path }
